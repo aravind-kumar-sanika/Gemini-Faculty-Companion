d691f953f01c8bb3279bde89ec57eddfb0fe6645
Deepthi Annem <deepthireddyannem.com>
2025-11-18 00:04:28 -0600

    Initial Spring Boot project

---
+++ src/main/java/com/GeminiFacultyCompanion/GeminiFacultyCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.GeminiFacultyCompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiFacultyCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiFacultyCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GeminiFacultyCompanion/GeminiFacultyCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GeminiFacultyCompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiFacultyCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
