54965490bf79500988b6f5deb7a1caed3774882e
Deepthi Annem <deepthireddyannem.com>
2025-11-26 16:00:41 -0600

    Organized project into backend and frontend folders

--- backend/src/main/java/com/GeminiFacultyCompanion/GeminiFacultyCompanionApplication.java
+++ backend/src/main/java/com/GeminiFacultyCompanion/GeminiFacultyCompanionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeminiFacultyCompanionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeminiFacultyCompanionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiFacultyCompanionApplication.class, args);
+    }
 
 }
